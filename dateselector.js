d64da85ef0ad6ebf5fd9967355f28bd682195901
gleb87 <gleb87@3g.ua>
2015-02-20 21:46:32 +0200

    update #1

--- dateselector.js
+++ dateselector.js
@@ -7,6 +7,66 @@
 function DateSelector(options) {
   var self = this;
 
+  var monthNames = "январь февраль март апрель май июнь июль август сентябрь октябрь ноябрь декабрь".split(" ");
+  var template = _.template(options.template);
+  var value = options.value;
 
-  /* ваш код */
+  var elem, yearElem, monthElem, dayElem;
+
+  function render() {
+  	elem = $(template({
+  		yearFrom: options.yearFrom,
+  		yearTo: options.yearTo,
+  		monthNames: monthNames,
+  		monthLength: getMonthLength(value),
+  	}));
+
+  	yearElem = elem.find(".year").change(onDateChange);
+  	monthElem = elem.find(".month").change(onDateChange);
+  	dayElem = elem.find(".day").change(onDateChange);
+
+  	selectValue();
+
+  	return elem;
+  }
+
+  function getMonthLength(date) {
+  	return ( new Date(date.getFullYear(), date.getMonth() + 1, 0) ).getDate();
+  }
+
+  self.getElement = function() {
+  	return elem || render();
+  }
+
+  self.setValue = function(date, quiet) {
+  	value = date;
+
+  	dayElem.empty();
+  	for (var i = 1; i <= getMonthLength(value); i++) {
+  		dayElem.append($("<option/>", {
+  			value: i,
+  			html: i,
+  		}));
+  	};
+  	
+  	selectValue();
+
+  	if (!quiet) {
+  		$(self).triggerHandler({
+  			type: "select",
+  			value: value,
+  		});
+  	};
+  }
+  
+  function selectValue() {
+  	yearElem.val(value.getFullYear());
+  	monthElem.val(value.getMonth());
+  	dayElem.val(value.getDate());
+  }
+
+  function onDateChange() {
+  	var newDate = new Date(yearElem.val(), monthElem.val(), dayElem.val());
+  	self.setValue(newDate);
+  }
 }
